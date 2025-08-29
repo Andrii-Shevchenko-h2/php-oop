9f8bdd3e9858c3f96b84be79e168016de0f758ad
Andrii-Shevchenko-h2 <219667769+Andrii-Shevchenko-h2@users.noreply.github.com>
2025-08-29 11:48:29 +0200

    feat: remove redundant AncientCustomer class

--- app/Tests/CustomerTests.php
+++ app/Tests/CustomerTests.php
@@ -4,30 +4,23 @@ declare(strict_types = 1);
 
 namespace App\Tests;
 
-use \App\Modern\Customer as ModernCustomer;
-use \App\Ancient\Customer as AncientCustomer;
+use \App\Modern\Customer;
 
 readonly class CustomerTests extends Tests {
   protected static function runTests() {
-    $angelo = ModernCustomer::create('Angelo Merte', '17.07.1954');
-    $olaf = ModernCustomer::create('Olaf Holz', '14.06.1958');
-    $hercules = AncientCustomer::create('Hercules', '12.08.1000');
+    $angelo = Customer::create('Angelo Merte', '17.07.1954');
+    $olaf = Customer::create('Olaf Holz', '14.06.1958');
+    $hercules = Customer::create('Hercules', '12.08.1000');
 
     $customerNumber = 0;
 
-    $generateCustomerTestText = function(ModernCustomer|AncientCustomer $customer) use (&$customerNumber) {
+    $generateCustomerTestText = function(Customer $customer) use (&$customerNumber) {
       $customerNumber++;
-      $customerName = $customer instanceof ModernCustomer
-        ? $customer->name
-        : $customer->getName();
-      $customerBirthDate = $customer instanceof ModernCustomer
-        ? $customer->birthDate
-        : $customer->getBirthDate();
 
       return <<< CUSTOMER_TEST
       CUSTOMER $customerNumber
-        Name: $customerName
-        Birth Date: $customerBirthDate
+        Name: {$customer->name}
+        Birth Date: {$customer->birthDate}
         Age: $customer->age
         Mail: {$customer->mail->address}
       ---
